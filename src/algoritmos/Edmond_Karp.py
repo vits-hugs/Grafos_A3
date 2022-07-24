4f6de12432e2dc75d33b332e76277c4029075215
Vitor Hugo <vitormarzarotto@hotmail.com>
2022-07-24 10:06:22 -0300

    mais partes do ed_karp

--- src/algoritmos/Edmond_Karp.py
+++ src/algoritmos/Edmond_Karp.py
@@ -15,12 +15,23 @@ def busca_largura_ed_Karp(grafo: Grafo_dirigido):
     while Q != []:
         u = Q.pop(0)
         for vertice in grafo.vizinhos_plus(u):
-            if C[vertice-1] == False:
-                print(vertice)
+            if C[vertice-1] == False and verify_aresta(grafo,u,vertice):
+                C[vertice-1] = True
+                A[vertice-1] = u
+
+                if vertice == grafo.vertices_array[-1]:
+                    p = [grafo.vertices_array[-1]]
+                    w = grafo.vertices_array[-1]
+                    while w!=grafo.vertices_array[0]:
+                        w = A[w-1]
+                        p.append(w)
+                    return p 
+                Q.append(vertice)
+            
 
 def Edmonds_Karp(grafo:Grafo_dirigido):
-    fluxo = [0]*grafo.qtdArestas()
     p = busca_largura_ed_Karp(grafo)
+    print(grafo.fluxo)
     while p != -1:
         pass
         #cf(p) = min(cf(u,v):(u,v) e p)
@@ -29,12 +40,20 @@ def Edmonds_Karp(grafo:Grafo_dirigido):
                 pass
             else:
                 pass
-            
 
+
+def verify_aresta(grafo:Grafo_dirigido,u,v):
+    ind =  grafo.get_index_aresta(u,v)
+    if ind == -1:
+        return 0
+    return grafo.arestas[ind].peso - grafo.fluxo[ind]
 
 g_dirigido = GrafoFactory().create_dirigido("dirigido.net")
 
 
 busca_largura_ed_Karp(g_dirigido)
+
+for x in g_dirigido.arestas:
+    print(x)
 
 
