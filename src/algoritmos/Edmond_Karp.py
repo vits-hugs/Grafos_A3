1f0819fc7e851b19923d22bba8f3e769338b82aa
Vitor Hugo <vitormarzarotto@hotmail.com>
2022-07-24 09:31:41 -0300

    pseudo_code edmonds-karp

--- src/algoritmos/Edmond_Karp.py
+++ src/algoritmos/Edmond_Karp.py
@@ -4,7 +4,8 @@ from grafo_dirigido import Grafo_dirigido
 from grafoFactory import GrafoFactory
 
 
-def Edmond_Karp(grafo: Grafo_dirigido):
+
+def busca_largura_ed_Karp(grafo: Grafo_dirigido):
     qtdV = grafo.qtdVertices()
     C = [False]*qtdV
     A = [None]*qtdV    
@@ -17,14 +18,23 @@ def Edmond_Karp(grafo: Grafo_dirigido):
             if C[vertice-1] == False:
                 print(vertice)
 
-
-
-
+def Edmonds_Karp(grafo:Grafo_dirigido):
+    fluxo = [0]*grafo.qtdArestas()
+    p = busca_largura_ed_Karp(grafo)
+    while p != -1:
+        pass
+        #cf(p) = min(cf(u,v):(u,v) e p)
+        for aresta in p:
+            if grafo.haAresta(aresta.inicio,aresta.fim):
+                pass
+            else:
+                pass
+            
 
 
 g_dirigido = GrafoFactory().create_dirigido("dirigido.net")
 
 
-Edmond_Karp(g_dirigido)
+busca_largura_ed_Karp(g_dirigido)
 
 
